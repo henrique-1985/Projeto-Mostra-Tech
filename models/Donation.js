300b02bdb1b8dca6a043cab9c4c2df35061ad5d7
[Caroline Vitória Vieira de Abreu] <carolinevv01@gmail.com>
2025-12-13 20:52:42 -0300

    Modelo pra exibição de dados

--- models/Donation.js
+++ models/Donation.js
@@ -15,17 +15,20 @@ const Donation = db.sequelize.define('donation', {
         allowNull: false
     }
 });
+
 /*
 Donation.create({
     itemType: 'Roupas',
     description: 'Camisas e calças em bom estado',
-    quantity: 20
+    donator:  'João Silva'
 }).then(() => {
     console.log('Item de doação criado com sucesso!');
 }).catch((error) => {
     console.error('Erro ao criar item de doação:', error);
 });
 */
+
+
 //Donation.sync({force:true}); //Descomente esta linha para criar a tabela no banco de dados, depois comente novamente
 
 module.exports = Donation;
