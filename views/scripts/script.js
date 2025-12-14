776a4d431517f1096fd52a8d54834c21859f0f93
henrique-1985 <henrique.nascimento9891@gmail.com>
2025-12-14 19:08:51 -0300

    Mudanças no cadastro e index

--- views/scripts/script.js
+++ views/scripts/script.js
@@ -1,27 +1,21 @@
 //Script para alternar entre os formulários de login e cadastro
-const botaoCadastrar = document.querySelector("#cadastrar");
-const botaoLogin = document.querySelector("#login");
-const formCadastrar = document.querySelector(".caixa-cadastrar");
+const trocarLogin = document.querySelector("#ir-login");
+const trocarCadastrar = document.querySelector("#ir-cadastrar");
 const formLogin = document.querySelector(".caixa-entrar");
+const formCadastrar = document.querySelector(".caixa-cadastrar");
 
-botaoCadastrar.addEventListener("click", () => { // Troca para o formulário de cadastro ao clicar no botão cadastrar
-  botaoCadastrar.style.backgroundColor = "#6aa3dd";
-  botaoLogin.style.backgroundColor = "rgba(255, 255, 255, 0.3)";
-
-  formCadastrar.style.left = "50%";
-  formLogin.style.left = "-50%";
-  
-  formCadastrar.style.opacity = "1";
-  formLogin.style.opacity = "0";
-})
-
-botaoLogin.addEventListener("click", () => { // Troca para o formulário de login ao clicar no botão entrar
-  botaoCadastrar.style.backgroundColor = "rgba(255, 255, 255, 0.3)";
-  botaoLogin.style.backgroundColor = "#6aa3dd";
-
+trocarLogin.addEventListener("click", () => { // Troca para o formulário de login ao clicar no botão entrar
   formCadastrar.style.left = "150%";
   formLogin.style.left = "50%";
   
   formCadastrar.style.opacity = "0";
   formLogin.style.opacity = "1";
+})
+
+trocarCadastrar.addEventListener("click", () => { // Troca para o formulário de cadastro ao clicar no botão cadastrar
+  formCadastrar.style.left = "50%";
+  formLogin.style.left = "-50%";
+  
+  formCadastrar.style.opacity = "1";
+  formLogin.style.opacity = "0";
 })
