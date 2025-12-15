161949650812c286c064c6ed975a17155fd07992
henrique-1985 <henrique.nascimento9891@gmail.com>
2025-12-14 23:26:27 -0300

    Edições no cadastro

--- views/scripts/script.js
+++ views/scripts/script.js
@@ -1,9 +1,12 @@
-//Script para alternar entre os formulários de login e cadastro
 const trocarLogin = document.querySelector("#ir-login");
 const trocarCadastrar = document.querySelector("#ir-cadastrar");
 const formLogin = document.querySelector(".caixa-entrar");
 const formCadastrar = document.querySelector(".caixa-cadastrar");
+const checkboxONG = document.querySelector("#tipo-usuario");
+const inputNome = document.querySelector('input[name="name"]');
+const inputEmail = document.querySelector('input[name="email"]');
 
+//Script para alternar entre os formulários de login e cadastro
 trocarLogin.addEventListener("click", () => { // Troca para o formulário de login ao clicar no botão entrar
   formCadastrar.style.left = "150%";
   formLogin.style.left = "50%";
@@ -19,3 +22,14 @@ trocarCadastrar.addEventListener("click", () => { // Troca para o formulário de
   formCadastrar.style.opacity = "1";
   formLogin.style.opacity = "0";
 })
+
+//Altera o placeholder do cadastro se for ONG
+checkboxONG.addEventListener("change", () => {
+  if (checkboxONG.checked) {
+    inputNome.placeholder = "Insira o nome da sua organização";
+    inputEmail.placeholder = "Insira o email da sua organização";
+  } else {
+    inputNome.placeholder = "Insira seu nome";
+    inputEmail.placeholder = "Insira seu email";
+  }
+});
