26cbb9ccc2656ee4c35d43b4d1203c64b0d620a6
henrique-1985 <henrique.nascimento9891@gmail.com>
2025-12-15 19:26:59 -0300

    Botão de resgate e caixa de popup se o usuário for ONG

--- views/scripts/barra-nav-script.js
+++ views/scripts/barra-nav-script.js
@@ -1,20 +1,25 @@
-//Script para carregar a barra de navegação
-fetch("../extras/barra-navegacao.html")
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById("link-navbar").innerHTML = data;
-    mostrarMenu();
-  })
-  .catch(error => {
-    console.error("Erro, não foi possível carregar a barra de navegação:", error);
+//Script para carregar os elementos dinâmicos
+function elementoDinamico(caminhoHtml, idElemento, callback) {
+  fetch(caminhoHtml)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById(idElemento).innerHTML = data;
+      callback?.();
   });
+}
 
 //Função para mostrar o menu ao clicar no ícone
 function mostrarMenu() {
-    const botaoMenu = document.getElementById("icone-menu");
-    const listaMenu = document.querySelector(".paginas-nav");
+  const botaoMenu = document.getElementById("icone-menu");
+  const listaMenu = document.querySelector(".paginas-nav");
     
-    botaoMenu.addEventListener("click", () => {
-        listaMenu.classList.toggle("aberto");
-    });
+  botaoMenu.addEventListener("click", () => {
+    listaMenu.classList.toggle("aberto");
+  });
 }
+
+//Chama a função para carregar a barra de navegação e a função de mostrar o menu
+elementoDinamico("../extras/barra-navegacao.html", "link-navbar", mostrarMenu);
+
+//Chama a função para carregar o footer
+elementoDinamico("../extras/", "");
--- views/scripts/doacoes-script.js
+++ views/scripts/doacoes-script.js
@@ -1,13 +1,42 @@
-//Script para alterar o que é exibido na página de doações
-document.addEventListener("DOMContentLoaded", () => {
-    const isONG = window.location.hash === "#ong"; // Verifica se a URL contém #ong
-    const divOng = document.getElementById("ong");  
-    const catalogo = document.getElementById("catalogo");
-    const btnAdicionar = document.getElementById("btn-adicionar");
+//Consts do popup de confirmação
+const resgatarDoacao = document.getElementById("btn-resgatar");
+const caixaPopup = document.getElementById("popup-resgatar");
+const textoPopup = document.getElementById("popup-txt");
+const iconePopup = document.getElementById("popup-icone");
+const confirmarResgate = document.getElementById("btn-confirmar");
+const cancelarResgate = document.getElementById("btn-cancelar");
+const fecharPopup = document.getElementById("btn-fechar");
+const botoesFechar = [cancelarResgate, fecharPopup];
 
-    if (isONG) { // Se for ONG, mostra a div da ONG e esconde a versão original
-        divOng.style.display = "block";
-        catalogo.style.display = "none";
-        btnAdicionar.style.display = "none";
-    }
+//Função para acionar a caixa de confirmação
+resgatarDoacao.addEventListener("click", () => {
+    caixaPopup.style.display = 'block';
+});
+
+//Função para confirmar o resgate, muda o texto, o ícone e os botões
+confirmarResgate.addEventListener("click", () => {
+  textoPopup.innerText = "Resgate confirmado!";
+  iconePopup.className = "bx bx-check-circle";
+  confirmarResgate.style.display = "none";
+  cancelarResgate.style.display = "none";
+  fecharPopup.style.display = "block";
+});
+
+//Função para fechar a caixa ao clicar nos botões de cancelar ou fechar
+botoesFechar.forEach(botao => {
+  botao.addEventListener("click", () => {
+    caixaPopup.style.display = 'none';
+  });
+});
+
+//Script para alterar o que é exibido na página de doações se for ONG ou não
+window.addEventListener("DOMContentLoaded", () => {
+  const isOng = window.location.hash === "#ong";
+  const mostrarBotao = document.querySelectorAll(".botao-resgatar");
+
+  if (isOng) {
+    mostrarBotao.forEach(botao => {
+      botao.style.display = "block";
+    });
+  }
 });
