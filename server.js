d9c4cc7deaae50716f5e45eb97a05f53f878bc34
[Caroline Vitória Vieira de Abreu] <carolinevv01@gmail.com>
2026-03-26 15:52:52 -0300

    UPDATE

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const segredo = "SegredoShareSpot123"; // Segredo para JWT
 
+
+function welcomeMessage() {
+    console.log("<-----------UNINASSAU BOA VIAGEM - ADS 2026.1--------->")
+    console.log("-> Reapresentação do ShareSpot")
+    console.log("<----------------------------------------------->")
+}
+
 const db = require('./models/db');
 const User = require('./models/User');  
 const Donation = require('./models/Donation');
@@ -54,9 +61,9 @@ app.post('/cadastro', async function (req, res) {
             isONG:req.body.isONG
         });
         res.redirect('/');
-        window.alert("Usuário cadastrado com sucesso!");
+        window.alert("> Usuário cadastrado com sucesso!");
     } catch (error) {
-        res.send("Erro ao cadastrar usuário: " + error);
+        return res.send("> Erro ao cadastrar usuário: ");
     }
 });
 
@@ -82,7 +89,7 @@ app.post('/donate', async function (req, res) {
             res.redirect('/doacoes');
         }
     } catch (error) {
-        res.send("Erro ao cadastrar item: " + error);
+        res.send("> Erro ao cadastrar item: " + error);
     }
 });
 
@@ -95,7 +102,7 @@ app.get('/resgatar/:id', async (req, res) => {
     }).then(() => {
         res.redirect('/doacoes#ong');
     }).catch(function(error){
-        res.send("Erro ao resgatar doação: " + error);
+        res.send("> Erro ao resgatar doação: " + error);
     });
 });
 /*
@@ -127,7 +134,7 @@ app.get('/usuarios', async (req, res) => {
         const users =  await User.findAll();
         res.render('routes/usuarios.ejs', { users });
     } catch (error){
-        res.send("Erro ao listar usuários: " + error);
+        res.send("> Erro ao listar usuários: " + error);
     }
 
    
@@ -145,7 +152,7 @@ app.get('/doacoes', async (req, res) => {
             res.render('routes/doacoes_teste.ejs', { donations });
         }
     } catch (error){
-        res.send("Erro ao listar doações: " + error);
+        res.send("> Erro ao listar doações: " + error);
     }
 
    
@@ -170,7 +177,7 @@ app.post('/login', async (req, res) =>{
         }
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
-            return res.status(400).send('Senha incorreta');
+            return res.status(400).send('> Senha incorreta! > Tente novamente.');
         }
         const token = gerarJwt(user); // Gerar o token JWT para o usuário autenticado
 
@@ -180,9 +187,55 @@ app.post('/login', async (req, res) =>{
         });
 
     } catch (error) {
-        res.status(500).send('Erro ao fazer login: ' + error);
+        res.status(500).send('> Erro ao fazer login: ' + error);
     }   
 });
+
+
+//Perfil do Usuário
+
+app.get('/perfil', async (req, res) => {
+    try {
+        if (parseInt(req.params.id) !== req.userId) {
+            return res.status(403).send('> Acesso negado! > Usuário não verificado ou token expirado!');
+        }
+        const usuario = await User.findByPk(req.userId);
+        if (!usuario) {
+            return res.status(404).send('> Usuário não encontrado!');
+        }
+        res.render('routes/perfil.html', { usuario });
+    } catch (error) {
+        res.status(500).send('> Erro ao buscar perfil: ' + error);
+    }
+});
+
+
+//Update
+
+
+app.post('/update-cadastro', autenticarToken, async (req, res) => {
+    try {
+
+        if (req.body.id != req.userId) {
+        return res.status(403).send('> Acesso negado! > Usuário não verificado ou token expirado!');
+        }
+        await User.update({
+            name:req.body.name,
+            email:req.body.email,
+            phone:req.body.phone,
+            password:req.body.password,
+        }, {
+            where: { id: req.userId }
+        });
+        return res.redirect('/');
+           
+    } catch(error){
+        return res.send("> Erro ao atualizar usuário: " + error);
+    }
+    
+});
+   
+
 
 //Funções de Token
 
@@ -192,9 +245,9 @@ function gerarJwt(user){ // Função para gerar o token JWT, recebe o usuário c
 }
 
 function autenticarToken(req, res, next) { //Função para autenticar o token JWT
-    const token = req.headers['authorization'];
+    const token = req.headers['authorization'] || req.body.token || req.query.token;
 
-    if (!token) return res.status(403).json({ message: 'Token não fornecido' });
+    if (!token) return res.status(403).json({ message: '> Token não fornecido!' });
 
     try {
         const verificar = jwt.verify(token, segredo);
@@ -202,7 +255,7 @@ function autenticarToken(req, res, next) { //Função para autenticar o token JW
         req.isONG = verificar.isONG;
         next();
     } catch (error) {
-        res.status(401).send('Token inválido');
+        res.status(401).send('> Token inválido!');
     }
 }
 
@@ -210,7 +263,8 @@ function autenticarToken(req, res, next) { //Função para autenticar o token JW
 
 try{
     app.listen(port);
-    console.log(`Servidor rodando em http://localhost:${port}` +'/');
+    welcomeMessage();
+    console.log(`> Servidor Iniciado! Acesse em: http://localhost:${port}` +'/');
 }catch(error){
-    console.log("Erro ao iniciar o servidor: " + error);
+    console.log("> Erro ao iniciar o servidor: " + error);
 }
