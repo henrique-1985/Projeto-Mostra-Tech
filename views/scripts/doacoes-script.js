daf23963be04a457da30c43236fb1068dea11a03
henrique-1985 <henrique.nascimento9891@gmail.com>
2026-03-31 16:53:07 -0300

    Edições finais

--- views/scripts/doacoes-script.js
+++ views/scripts/doacoes-script.js
@@ -1,5 +1,5 @@
 //Consts do popup de confirmação
-const resgatarDoacao = document.getElementById("btn-resgatar");
+const resgatarDoacao = document.querySelectorAll(".botao-resgatar");
 const caixaPopup = document.getElementById("popup-resgatar");
 const textoPopup = document.getElementById("popup-txt");
 const iconePopup = document.getElementById("popup-icone");
@@ -9,8 +9,10 @@ const fecharPopup = document.getElementById("btn-fechar");
 const botoesFechar = [cancelarResgate, fecharPopup];
 
 //Função para acionar a caixa de confirmação
-resgatarDoacao.addEventListener("click", () => {
-    caixaPopup.classList.add("aberto");
+resgatarDoacao.forEach(btn => {
+    btn.addEventListener("click", () => {
+        caixaPopup.classList.add("aberto");
+    });
 });
 
 //Função para confirmar o resgate, muda o texto, o ícone e os botões
@@ -27,23 +29,4 @@ botoesFechar.forEach(botao => {
   botao.addEventListener("click", () => {
     caixaPopup.classList.remove("aberto")
   });
-});
-
-//Script que altera a visibilidade dos botões de resgatar e adicionar doações dependendo do tipo de usuário
-document.addEventListener("DOMContentLoaded", () => {
-    const user = JSON.parse(localStorage.getItem("user"));
-    const resgatarDoacao = document.querySelectorAll(".botao-resgatar");
-    const adicionarDoacao = document.querySelectorAll(".btn-adicionar");
-
-    resgatarDoacao.forEach(botao => botao.style.display = "none");
-    adicionarDoacao.forEach(botao => botao.style.display = "none");
-
-    if (user) {
-        if (user.isONG) {
-            window.location.hash = "ong";
-            resgatarDoacao.forEach(botao => botao.style.display = "block");
-        } else {
-            adicionarDoacao.forEach(botao => botao.style.display = "block");
-        }
-    }
 });
